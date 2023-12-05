047c7e62b275400d50750b3233bef1a7393c3c28
Jakub Nowaczek <j.nowaczek30@gmail.com>
2023-12-05 22:15:06 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-<<<<<<< Updated upstream
-    baseURL: 'https://fullstackfooddeliveryserver-production.up.railway.app'
-=======
     baseURL: 'https://fullstackfooddeliveryapp-production.up.railway.app' || 'http://localhost:5000/'
->>>>>>> Stashed changes
 });
